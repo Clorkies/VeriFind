a1864710f5da446030fde689ec0d26d3112cc3e3
Clorkies <clarkisnoob434@gmail.com>
2026-05-26 00:19:30 +0800

    feat: Remove Admin link from NavBar component

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -66,7 +66,6 @@ export function NavBar() {
         <nav className="hidden items-center gap-1 md:flex">
           <NavLink href="/board">Board</NavLink>
           <NavLink href="/my-claims">My Claims</NavLink>
-          <NavLink href="/admin">Admin</NavLink>
           <NavLink href="/#how-it-works">How it works</NavLink>
         </nav>
       </div>
