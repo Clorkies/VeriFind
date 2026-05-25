14cbc3a3342fa86ed078b101f65382ef131c588f
Clorkies <clarkisnoob434@gmail.com>
2026-05-25 19:10:48 +0800

    feat: Remove wallet and qr flow

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { WalletProvider } from "@/app/context/WalletProvider";
 import { ItemsProvider } from "@/app/context/ItemsProvider";
 import type { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ItemsProvider>
-      <WalletProvider>{children}</WalletProvider>
-    </ItemsProvider>
+    <ItemsProvider>{children}</ItemsProvider>
   );
 }
